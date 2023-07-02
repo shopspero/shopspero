06fb973c710e374bc3c8aab8b9e48d537c4c8cba
Jeffrey Liang <jeffrey.liang123@gmail.com>
2023-07-02 15:50:51 -0700

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { Box, Container, Center, Heading } from '@chakra-ui/react';
+
+export default function NotFound() {
+  return (
+    <Box>
+      <Center pt={10} pb={10} pl={5} pr={5}>
+        <Heading as="h1" size="2xl" textAlign="center">
+          Uh oh, 404.
+        </Heading>
+      </Center>
+      <Container maxWidth={900} p={5} textAlign="center">
+        Looks like we couldn't find the page you were looking for :(
+      </Container>
+    </Box>
+  );
+}
