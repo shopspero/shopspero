a9bc409ea42507b6c008125a5af21ec4f108967d
Jeffrey Liang <jeffrey.liang123@gmail.com>
2023-07-04 17:00:35 -0700

    Add redirects for /about and /interest

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   experimental: {
     mdxRs: true,
   },
+  redirects: () => {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/interest',
+        destination:
+          'https://docs.google.com/forms/d/e/1FAIpQLSdpUESCiXh5c-L7aDgN05G7kL9za8kwqom_LdGLUiG1jBqA2A/viewform',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const withMDX = require('@next/mdx')();
