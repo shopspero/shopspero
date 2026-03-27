871f76ac6a711114e58863a8e6653293d7f01a6e
Michael Dai <michael_dai@berkeley.edu>
2026-03-27 15:44:47 -0700

    model specs in Lux

--- src/app/(external)/shop/ShopLux.tsx
+++ src/app/(external)/shop/ShopLux.tsx
@@ -4,7 +4,7 @@ import Product from '../../../components/Product'
 function Description() {
   return (
     <div>
-      <p>Through His saving work, God brings His presence back to His people and calls us out of darkness into His marvelous light (1 Peter 2:9). Isaiah declares that “the Lord will be your everlasting light” (Isaiah 60:20) and that His people will be made righteous by “the work of my hands, that I might be glorified” (Isaiah 60:21). Our hearts thrill with joy because of what Christ has done for us, yet we stand before Him with awe and reverence. Lux Aeterna is a reminder to lift our eyes to the everlasting light of Christ and live as reflections of His glory.</p>
+      <p>Through His saving work, God brings His presence back to His people and calls us out of darkness into His marvelous light (1 Peter 2:9). Isaiah declares that “the Lord will be your everlasting light” (Isaiah 60:20) and that His people will be made righteous by “the work of my hands, that I might be glorified” (Isaiah 60:21). Our hearts thrill with joy because of what Christ has done for us, yet we stand before Him with awe and reverence. Lux Aeterna is a reminder to lift our eyes to the everlasting light of Christ and live as reflections of His glory. Model is 5'10 wearing a size L.</p>
     </div>
   )
 } 
